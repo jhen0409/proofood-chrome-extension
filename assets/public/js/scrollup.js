41e674f625c45d0c42d02fc476a239533f374a49
Jhen-Jie Hong <iainst0409@gmail.com>
2015-02-01 11:36:29 +0800

    nav-tab style

--- assets/public/js/scrollup.js
+++ assets/public/js/scrollup.js
@@ -1,18 +1,17 @@
 $(document).ready(function () {
 
-    $(window).scroll(function () {
-        if ($(this).scrollTop() > 100) {
-            $('.scrollup').fadeIn();
-        } else {
-            $('.scrollup').fadeOut();
-        }
-        console.log
-    });
+	$(window).scroll(function () {
+		if ($(this).scrollTop() > 100) {
+			$('.scrollup').fadeIn();
+		} else {
+			$('.scrollup').fadeOut();
+		}
+	});
 
-    $('.scrollup').click(function () {
-        $("html, body").animate({
-            scrollTop: 0
-        }, 600);
-        return false;
-    });
+	$('.scrollup').click(function () {
+		$("html, body").animate({
+			scrollTop: 0
+		}, 600);
+		return false;
+	});
 });
