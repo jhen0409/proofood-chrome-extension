e711d491b6b4151ca0d5804e7393c574dd1224c6
Jhen-Jie Hong <iainst0409@gmail.com>
2015-02-07 14:41:18 +0800

    pagination

--- assets/public/js/index.js
+++ assets/public/js/index.js
@@ -3,6 +3,7 @@ var app = angular.module("proofood", []);
 
 app.controller('ProofoodCtrl', function($scope, $http, $sce) {
 	$scope.noData = false;
+	$scope.selectedTab = 0;
 	var parseQueryString = function() {
 		var str = window.location.search;
 		var objURL = {};
@@ -15,13 +16,30 @@ app.controller('ProofoodCtrl', function($scope, $http, $sce) {
 	var query = parseQueryString();
 	
 	var searchKeyword = function(keyword) {
-		$http.get('http://proofood.tw/search.json?q=' + keyword).then(function(resp) {
+		$scope.data = undefined;
+		$scope.items = undefined;
+		$scope.pages = undefined;
+		$http.get('http://proofood.tw/search.json?' + keyword).then(function(resp) {
 			if (resp.status == 200) {
 				$scope.data = [];
 				$scope.items = [];
+				$scope.pages = [];
 				_.each(resp.data, function(product) {
 					$scope.items.push({ val: Object.keys(product)[0] });
 					$scope.data.push(product[ Object.keys(product)[0] ]);
+					var page = {
+						total_pages: product.total_pages,
+						total_count: product.total_count,
+						current_page: product.current_page,
+						pagination: []
+					};
+					var start = page.current_page - 4, goal = page.current_page + 4;
+					for (var i = start; i <= goal; i++) {
+						if (i > 0 && i <= page.total_pages) {
+							page.pagination.push(i);
+						}
+					}
+					$scope.pages.push(page);
 				});
 				$scope.noData = $scope.data.length == 0;
 			}
@@ -35,7 +53,7 @@ app.controller('ProofoodCtrl', function($scope, $http, $sce) {
 		console.log($scope.q);
 		$scope.result = $scope.q.replace(/[,\s%2C]+/g, ' ');
 
-		searchKeyword($scope.q);
+		searchKeyword('q=' + $scope.q);
 	} else {
 		$scope.data = [];
 		$scope.noData = true;
@@ -59,6 +77,14 @@ app.controller('ProofoodCtrl', function($scope, $http, $sce) {
 		});
 	}
 
+	function genSearch(changeKey, pageIndex) {
+		var search = [];
+		_.each($scope.items, function(item, i) {
+			search.push(item.val + 'page=' + (changeKey == item.val ? pageIndex : $scope.pages[i].current_page));
+		});
+		return search.join('&');
+	}
+
 	// 過濾掉 ',' and '■', 並將 url 轉成 link
 	$scope.replaceSellChannel = function(str) {
 		str = str.replace(/(<([^>]+)>)/ig, '')
@@ -67,5 +93,29 @@ app.controller('ProofoodCtrl', function($scope, $http, $sce) {
 
 		return $sce.trustAsHtml(replaceURLWithHTMLLinks(str));
 	}
+
+	$scope.prevTabPage = function(tabIndex) {
+		if ($scope.pages[tabIndex].current_page == 1) {
+			return;
+		}
+		searchKeyword('q=' + $scope.q + '&' + genSearch($scope.items[tabIndex].val, $scope.pages[tabIndex].current_page - 1 ));
+		$scope.selectedTab = tabIndex;
+	};
+
+	$scope.toTabPage = function(tabIndex, page) {
+		if (page < 1 || page > $scope.pages[tabIndex].total_pages) {
+			return;
+		}
+		searchKeyword('q=' + $scope.q + '&' + genSearch($scope.items[tabIndex].val, page ));
+		$scope.selectedTab = tabIndex;
+	};
+
+	$scope.nextTabPage = function(tabIndex) {
+		if ($scope.pages[tabIndex].current_page == $scope.pages[tabIndex].total_pages) {
+			return;
+		}
+		searchKeyword('q=' + $scope.q + '&' + genSearch($scope.items[tabIndex].val, $scope.pages[tabIndex].current_page + 1 ));
+		$scope.selectedTab = tabIndex;
+	};
 });
 
